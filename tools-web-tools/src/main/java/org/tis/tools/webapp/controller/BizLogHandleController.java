43957f461cc1904746c40df544f8f10ffaa04d14
shiyunlai <shiyunlai@gmail.com>
2016-11-17 09:25:34 +0800

    清理

--- tools-web-tools/src/main/java/org/tis/tools/webapp/controller/BizLogHandleController.java
+++ tools-web-tools/src/main/java/org/tis/tools/webapp/controller/BizLogHandleController.java
@@ -9,9 +9,6 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -22,6 +19,9 @@ import org.tis.tools.base.web.util.AjaxUtils;
 import org.tis.tools.service.api.biztrace.BiztraceFileInfo;
 import org.tis.tools.service.api.biztrace.IBiztraceRService;
 import org.tis.tools.service.api.biztrace.ParseProcessInfo;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 
 @Controller
