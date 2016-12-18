39aedded6575d557738fa25c9c32f048f2fdcd73
iiRGB <179494015@qq.com>
2016-12-18 19:16:54 +0800

    完善通用controller调用功能
    完善通用controller调用功能

---
+++ tools-web-tools/src/main/java/org/tis/tools/webapp/controller/ComCallerTest.java
@@ -0,0 +1,106 @@
+package org.tis.tools.webapp.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.tis.tools.base.web.controller.BaseController;
+import org.tis.tools.base.web.util.AjaxUtils;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+@Controller
+@RequestMapping("/commonCaller")
+public class ComCallerTest extends BaseController {
+
+	/**
+	 * postController测试方法
+	 * @param content
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/testPostController",method=RequestMethod.POST)
+	public String testPostController(@RequestBody String content, HttpServletRequest request,
+			HttpServletResponse response) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		try {
+			if (logger.isInfoEnabled()) {
+				logger.info("testPostController request : " + content);
+			}
+			JSONObject jsonObj = JSONObject.fromObject(content);
+			//{"type":"part","logs":["biztrace.log.1","biztrace.log.2"]}
+			String type = jsonObj.getString("type");
+			
+			//String[] strArr = (String[]) jsonObj.get("logs");
+			
+			List<String> list = new ArrayList<>();
+			JSONArray jsonArray = jsonObj.getJSONArray("logs");
+			for(int i = 0; i < jsonArray.size(); i ++) {
+				 String str = jsonArray.getString(i);
+				 list.add(str);
+			}
+			result.put("type", type);
+			result.put("logs", list);
+			AjaxUtils.ajaxJsonSuccessMessage(response, JSONArray.fromObject(result).toString());
+			
+		} catch (Exception e) {
+			AjaxUtils.ajaxJsonErrorMessage(response, "测试异常！");
+			logger.error("testPostController exception : ", e);
+		}
+		return null;
+	}
+	/**
+	 * testController测试方法
+	 * @param content
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/testGetController",method=RequestMethod.GET)
+	public String testGetController(HttpServletRequest request,
+			HttpServletResponse response) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		try {
+			if (logger.isInfoEnabled()) {
+				logger.info("testGetController request " );
+			}
+			String id = request.getParameter("id");
+			String name = request.getParameter("name");
+			result.put("id", id);
+			result.put("name", name);
+			AjaxUtils.ajaxJsonSuccessMessage(response, JSONArray.fromObject(result).toString());
+			
+		} catch (Exception e) {
+			AjaxUtils.ajaxJsonErrorMessage(response, "测试异常！");
+			logger.error("testGetController exception : ", e);
+		}
+		return null;
+	}
+	
+	
+	
+	private Map<String, Object> responseMsg ;
+	@Override
+	public Map<String, Object> getResponseCache() {
+		responseMsg = new HashMap<String, Object> () ;
+		// TODO Auto-generated method stub
+		return responseMsg;
+	}
+	
+
+}
--- tools-web-tools/src/main/webapp/tools/js/controllers/common/commonCaller_controller.js
+++ tools-web-tools/src/main/webapp/tools/js/controllers/common/commonCaller_controller.js
@@ -18,18 +18,53 @@ MetronicApp.controller('commonCaller_controller', function ($filter,$rootScope, 
      */
     $scope.requestInfo.host = 'localhost';
     $scope.requestInfo.port = '8089';
-    $scope.requestInfo.mapping = '/tis/log/analyse/172.20.10.9:20883';
+    //$scope.requestInfo.mapping = '/tis/log/analyse/172.20.10.9:20883';
+    $scope.requestInfo.mapping = '';
+    $scope.requestInfo.app_name = '/tis';
+    $scope.requestInfo.parent_mapping = '/commonCaller';
+    $scope.requestInfo.child_mapping = '/testPostController';
     $scope.requestInfo.protocol = 'http';
     $scope.requestInfo.method = 'POST';
     $scope.requestInfo.postJsonData = "{\"type\":\"part\",\"logs\":[\"biztrace.log.1\",\"biztrace.log.2\"]}";
-
+    $scope.requestInfo.getParamData = [];
     $scope.finalUrl= assemblyUrl(requestInfo);
 
-    $scope.responseJsonData= "" ;
+    $scope.responseJsonData = "";
 
+    var paramValues = [];
+    var paramItem1 = {};
+    paramItem1.paramKey = 'id';
+    paramItem1.paramVal = '123';
+    paramValues.push(paramItem1);
+    var paramItem2 = {};
+    paramItem2.paramKey = 'name';
+    paramItem2.paramVal = 'Jack';
+    paramValues.push(paramItem2);
+
+
+
+    $scope.paramValues = paramValues;
+    $scope.requestInfo.getParamData = paramValues;
+    $scope.addParamItem = function() {
+        var paramItem = {};
+        paramValues.push(paramItem);
+    }
+    $scope.delParamItem = function(index) {
+        paramValues.splice(index,1);
+    }
+    //$scope.addParamItem();
 
     //requestInfo 任何一个属性变化,都更新finalUrl
     var watch = $scope.$watch('requestInfo',function(newInfo,oldInfo, scope){
+
+
+        $scope.finalUrl= assemblyUrl(newInfo);
+
+    },true);
+
+    //requestInfo 任何一个属性变化,都更新finalUrl
+    var watch = $scope.$watch('requestInfo',function(newInfo,oldInfo, scope){
+
 
         $scope.finalUrl= assemblyUrl(newInfo);
 
@@ -37,11 +72,12 @@ MetronicApp.controller('commonCaller_controller', function ($filter,$rootScope, 
 
     // call按钮事件
     $scope.btnCall = function( requestInfo ){
+        $scope.responseJsonData = undefined;
         console.log( requestInfo ) ;
         var promise = commonCaller_service.callTest( requestInfo ) ;
         promise.then(function(data){
             console.log( data ) ;
-            $scope.responseJsonData = data ;
+            $scope.responseJsonData = data.data ;
         })
 
     }
