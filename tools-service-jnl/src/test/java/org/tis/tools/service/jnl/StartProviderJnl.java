a38a7b0d3de1cf07a557fdad0c34a64ab11eeb3d
shiyunlai <shiyunlai@gmail.com>
2017-03-21 14:39:14 +0800

    tools:gen-dao生成代码
    cd tools-core
    mvn tools:gen-dao -Dmodel.file.type=erm -Dfixed.bizmodels=jnl
    
    扫描工程中 model/ 目录下，所有*.erm 模型定义文件，只生成其中jnl的应用领域

---
+++ tools-service-jnl/src/test/java/org/tis/tools/service/jnl/StartProviderJnl.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.tis.tools.service.jnl;
+
+/**
+ * 
+ * 测试启动类
+ * 
+ * 在eclipse中启动本模块时使用
+ * 
+ * @author megapro
+ *
+ */
+public class StartProviderJnl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//extRouter() ;
+		com.alibaba.dubbo.container.Main.main(args);
+	}
+
+}
