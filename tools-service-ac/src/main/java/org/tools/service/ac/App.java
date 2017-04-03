ca240b6e5c8728e2ecb702cc1b215d23dd30fd19
shiyunlai <shiyunlai@gmail.com>
2017-04-04 07:28:15 +0800

    增加om、ac两个模块

---
+++ tools-service-ac/src/main/java/org/tools/service/ac/App.java
@@ -0,0 +1,13 @@
+package org.tools.service.ac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
