90e9855e930eead8ea26f93af1a1469862484366
shiyunlai <shiyunlai@gmail.com>
2017-04-11 07:54:13 +0800

    分类AC接口

---
+++ tools-facade-ac/src/main/java/org/tis/tools/rservice/ac/capable/IAuthenticatorRService.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.tis.tools.rservice.ac.capable;
+
+/**
+ * <pre>
+ * 
+ * 认证服务.
+ * 
+ * 认证就是核实用户身份的过程，
+ * 当操作员发生操作时，系统执行安全认证控制处理。
+ * 
+ * 提供的服务包括：
+ * <li>登陆
+ * <li>注销
+ * <li>修改操作员密码
+ * <li>检查操作权限
+ * <li>.....
+ * 
+ * </pre>
+ * @author megapro
+ *
+ */
+public interface IAuthenticatorRService {
+
+}
---
+++ tools-facade-ac/src/main/java/org/tis/tools/rservice/ac/capable/IRoleRService.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.tis.tools.rservice.ac.capable;
+
+/**
+ * 
+ * <pre>
+ * 
+ * 角色（Role）管理.</br>
+ * 
+ * 关于“角色”：
+ * <li>角色是功能的权限集合；
+ * <li>权限集定义：指定角色与功能（权限资源）间的关联关系；
+ * <li>权限赋值：及授权过程，将角色与被授权者建立关联，把功能权限赋值给组织对象（机构、工作组、职务、岗位、操作员）；
+ * 
+ * </pre>
+ * @author megapro
+ *
+ */
+public interface IRoleRService {
+	
+}
