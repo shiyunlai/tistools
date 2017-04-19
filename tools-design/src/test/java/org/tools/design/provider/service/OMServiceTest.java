19147fa52a6522377ccab2761e22a8ff6654349e
shiyunlai <shiyunlai@gmail.com>
2017-04-19 08:29:21 +0800

    增加测试用例
    orgRService.createRootOrg

--- tools-design/src/test/java/org/tools/design/provider/service/OMServiceTest.java
+++ tools-design/src/test/java/org/tools/design/provider/service/OMServiceTest.java
@@ -3,6 +3,7 @@
  */
 package org.tools.design.provider.service;
 
+import org.tis.tools.model.po.om.OmOrg;
 import org.tis.tools.rservice.om.capable.IOrgRService;
 
 /**
@@ -30,14 +31,21 @@ public class OMServiceTest {
 
 	public void start() throws Exception {
 
-		testGenOrgCode() ;
+		String newOrgCode = testGenOrgCode() ;
+		testCreateRootOrg(newOrgCode) ;
 	}
 
 	/**
 	 * 测试：获取机构代码
 	 */
-	private void testGenOrgCode() {
+	private String testGenOrgCode() {
 		String orgCode = orgRService.genOrgCode("021", "4", "90");
-		System.out.println("orgCode is :" + orgCode);		
+		System.out.println("orgCode is :" + orgCode);
+		return orgCode ; 
+	}
+	
+	private void testCreateRootOrg(String orgCode) {
+		OmOrg rootOrg = orgRService.createRootOrg(orgCode, "OMServiceTest", "90", "CN") ;
+		System.out.println("新根节点：\n"+rootOrg);
 	}
 }
