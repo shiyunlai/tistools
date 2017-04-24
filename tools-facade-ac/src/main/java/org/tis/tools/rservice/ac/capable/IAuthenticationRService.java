662847297964cb563c6bcd29819b882d7100e351
shiyunlai <shiyunlai@gmail.com>
2017-04-24 23:15:06 +0800

    增加权限设计

--- tools-facade-ac/src/main/java/org/tis/tools/rservice/ac/capable/IAuthenticationRService.java
+++ tools-facade-ac/src/main/java/org/tis/tools/rservice/ac/capable/IAuthenticationRService.java
@@ -8,7 +8,7 @@ package org.tis.tools.rservice.ac.capable;
  * 
  * 认证服务.
  * 
- * 认证就是核实用户身份的过程，
+ * 认证就是核实用户身份的过程，证明用户身份.
  * 当操作员发生操作时，系统执行安全认证控制处理。
  * 
  * 提供的服务包括：
@@ -22,6 +22,6 @@ package org.tis.tools.rservice.ac.capable;
  * @author megapro
  *
  */
-public interface IAuthenticatorRService {
+public interface IAuthenticationRService {
 
 }
