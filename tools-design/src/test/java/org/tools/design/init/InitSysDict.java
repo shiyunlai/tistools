1dd42cba7d1f515bc87faaecd0d6dede842f566a
shiyunlai <shiyunlai@gmail.com>
2017-05-14 11:53:27 +0800

    异常码定义文件位置变更，以便程序能正常加载异常码定义

---
+++ tools-design/src/test/java/org/tools/design/init/InitSysDict.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.tools.design.init;
+
+/**
+ * 
+ * 初始化业务字典
+ * 
+ * @author megapro
+ *
+ */
+public class InitSysDict {
+
+	/*
+	 * 解析excel获得业务字典和业务字典项
+	 * 
+	 * 然后调用远程服务插入数据库
+	 * 
+	 */
+	
+}
