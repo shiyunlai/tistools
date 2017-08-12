0b22c899182da866ef620a667d821604ededaae9
iiRGB <179494015@qq.com>
2017-08-12 08:08:32 +0800

    解决web不能获取异常信息

--- tools-core-basic/src/main/java/org/tis/tools/base/dubbo/optimizer/SerializationOptimizerImpl.java
+++ tools-core-basic/src/main/java/org/tis/tools/base/dubbo/optimizer/SerializationOptimizerImpl.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.tis.tools.AcRole;
+import org.tis.tools.base.exception.ToolsRuntimeException;
 import org.tis.tools.model.po.ac.AcOperator;
 
 import com.alibaba.dubbo.common.serialize.support.SerializationOptimizer;
@@ -39,6 +40,7 @@ public class SerializationOptimizerImpl implements SerializationOptimizer {
 		List<Class> classes = new LinkedList<Class>();
 		classes.add(AcOperator.class);
 		classes.add(AcRole.class);
+		classes.add(ToolsRuntimeException.class);
 		return classes;
 	}
 
