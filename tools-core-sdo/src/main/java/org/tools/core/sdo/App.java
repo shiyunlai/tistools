0f510e3ed2e728cb9d393ae8af20c21f9fb338f7
shiyunlai <shiyunlai@gmail.com>
2017-10-03 07:49:53 +0800

    新增SDO基础模块
    SDO，服务数据对象，用来在服务之间传递通用的数据结构

---
+++ tools-core-sdo/src/main/java/org/tools/core/sdo/App.java
@@ -0,0 +1,13 @@
+package org.tools.core.sdo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tools-core-sdo/src/main/java/org/tools/core/sdo/DataObject.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.tools.core.sdo;
+
+/**
+ * 
+ * 数据对象
+ * 
+ * @author megapro
+ *
+ */
+public class DataObject {
+
+}
