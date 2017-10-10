b83a036bd8b597d27487aa3bbf773d1452450667
shiyunlai <shiyunlai@gmail.com>
2017-10-10 09:19:53 +0800

    注释

--- tools-design/src/test/java/org/tools/design/test/util/ClassUtilAllTest.java
+++ tools-design/src/test/java/org/tools/design/test/util/ClassUtilAllTest.java
@@ -14,6 +14,9 @@ import org.junit.runners.Suite.SuiteClasses;
  *
  */
 
+// 使用Suite，把分散在多个工程中、且是对同一个被测对象的单元测试 集成起来
+// 也可以用Suite 集成存在逻辑关联性的一组单元测试，
+// 通过Suite一次执行所有相关的单元测试程序
 @RunWith(Suite.class)
 @SuiteClasses({ 
 	org.tools.design.test.util.ClassUtilTest.class, 
